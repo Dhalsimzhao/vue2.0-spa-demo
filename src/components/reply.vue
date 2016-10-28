892d17af08e37e6ccda5e89a8bab5660affc914d
dhalsimzhao <zhaolu880226@163.com>
2016-10-28 23:57:02 +0800

    ~

---
+++ src/components/reply.vue
@@ -0,0 +1,35 @@
+<style>
+  
+</style>
+
+<template>
+  <div class="replies">
+    <div class="replies-header">
+      {{ count }} 条回复
+    </div>
+    <div class="replies-list">
+      <ul>
+        <li v-for="reply in replies"></li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      count: Number,
+      replies: []
+    },
+
+    data() {
+      return {
+
+      }
+    },
+
+    mounted() {
+      console.log(this.reply_count)
+    }
+  }
+</script>
--- src/views/article.vue
+++ src/views/article.vue
@@ -1,11 +1,50 @@
 <template>
-  <div>
-    <h3>single article vue</h3>
+  <div class="article">
+    <div class="article-header"></div>
+    <!-- <div class="article-content" v-html="article.content"></div> -->
+    <reply :count="article.reply_count"
+           :replies="article.replies"></reply>
   </div>
 </template>
 
 <script type="text/javascript">
+import $ from 'jquery'
+import reply from '../components/reply'
 export default {
-  
+  filters: {
+  },
+
+  data() {
+    return {
+      articleId: '',
+      article: {}
+    }
+  },
+
+  mounted() {
+    console.log('article vue mounted');
+    this.getArticle()
+  },
+
+  methods: {
+    getArticle() {
+      // console.log('get article', this.$route)
+      this.articleId = this.$route.params.id;
+      let url = 'https://cnodejs.org/api/v1/topic/' + this.articleId;
+      $.getJSON(url, this.searchParams, json => {
+        this.article = json.data;
+      })
+    }
+  },
+
+  watch: {
+    '$route' (to, from) {
+      console.log(arguments);
+    }
+  },
+
+  components: {
+    reply
+  }
 }
 </script>
